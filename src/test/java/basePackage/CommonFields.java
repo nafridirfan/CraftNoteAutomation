123234b3ef31feb1c93f1af786f6d62c602b0642
nafridirfan <irfan.daval@gmail.com>
2020-07-19 04:26:30 +0200

    CommonFields.java has all the String literals. Sole purpose is to keep the code clean and tidy

---
+++ src/test/java/basePackage/CommonFields.java
@@ -0,0 +1,26 @@
+package basePackage;
+
+public class CommonFields {
+	
+	// iOS values to set the desired capabilities. 
+	public static String iPlatformName = "iOS";
+	public static String iPlatformVerson = "12.2";
+	public static String iAutomationName = "XCUITest";
+	public static String iBrowserName = "Safari";
+	public static String iDeviceName = "iPhone 8 Simulator";
+	
+	//Android values to set the desired capabilities. 
+	public static String platformName = "Android";
+	public static String platformVersion = "8";
+	public static String automationName = "UIAutomator2";
+	public static String browserName = "Chrome";
+	public static String deviceName = "Samsung Galaxy S9 Plus FHD GoogleAPI Emulator";
+	
+	//Server Names
+	public static String APPIUM_SERVER = "http://127.0.0.1:4723/wd/hub";
+	public static String SAUCELABS_SERVER = "https://nafridirfan:dc996e1a-af26-4ded-a247-3caed4774fda@ondemand.eu-central-1.saucelabs.com:443/wd/hub";
+	
+	//System Under Test
+	public static String SUT = "https://app.mycraftnote.de/";
+
+}
